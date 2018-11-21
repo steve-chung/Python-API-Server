851673e80310820cff811967f5b88a8ecc361d99
steve-chung <heeman_1@hotmail.com>
2018-11-20 21:27:05 -0800

    #3 added yelp api to get golf course info

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Api
 from yelpapi import YelpAPI
 import os
@@ -12,8 +12,6 @@ load_dotenv(dotenv_path)
 yelp_api_key = os.getenv('YELPKEY')
 PORT = os.getenv('PORT')
 yelp_api = YelpAPI(yelp_api_key)
-print(PORT)
-print(yelp_api_key)
 
 # from security import authenticate, identity
 # from resources.user import UserRegister
@@ -27,12 +25,37 @@ app = Flask(__name__)
 # app.secret_key = 'jose'
 api = Api(app)
 
+@app.errorhandler(500)
+def server_error(error=None):
+    message = {
+        'status': 500,
+        'message': 'Internal Server Error' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 500
+
+    return resp
 
 # @app.before_first_request
 # def create_tables():
 #     db.create_all()
 
+@app.route('/api/courses')
+def yelp_get_data():
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
 
+    response = yelp_api.search_query(
+            latitude = lat,
+            longitude = lng,
+            radius = 25000,
+            categories = 'golf',
+            sort_by = 'distance'
+    )
+    if response :
+        return jsonify(response)
+    return server_error()
+    
 # jwt = JWT(app, authenticate, identity)  # /auth
 
 # api.add_resource(Store, '/store/<string:name>')
