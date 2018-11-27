e5eb77975db59139fe213601c6caa3e23fedc571
steve-chung <heeman_1@hotmail.com>
2018-11-26 21:17:17 -0800

    deleted binary files

---
+++ models/holes.py
@@ -0,0 +1,26 @@
+from db import db
+from models.games import Game
+from sqlalchemy.exc import SQLAlchemyError
+from ast import literal_eval
+
+class Holes(db.Model):
+  __tablename__ = 'holes'
+
+  id = db.Column(db.Integer, primary_key=True)
+  game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+  hole_number = db.Column(db.Integer, nullable=False)
+  par = db.Column(db.Integer, nullable=False)
+
+
+  def __init__(self, game_id, hole_number, par):
+    self.game_id = game_id
+    self.hole_number = hole_number
+    self.par = par
+
+  def save_to_db(self):
+    try:
+      db.session.add(self)
+      db.session.commit()
+      return {'message': 'successfully insert a hole'}
+    except SQLAlchemyError as e:
+      print(e)
---
+++ resources/holes.py
@@ -0,0 +1,40 @@
+from flask_restful import Resource, reqparse
+from flask import jsonify
+from flask_jwt_extended import jwt_required, fresh_jwt_required
+from models.user import UserModel
+from models.games import Game
+from models.holes import Holes
+from ast import literal_eval
+
+
+parser = reqparse.RequestParser()
+parser.add_argument(
+    'game_id', help='This field cannot be blank', required=True)
+parser.add_argument(
+    'holes', action='append', help='This field cannot be blank', required=True)
+
+
+class createHoles(Resource):
+  @fresh_jwt_required
+  def post(self):
+    data = parser.parse_args()
+    holes = data['holes']
+    try: 
+      for hole in holes:
+        converted_hole = literal_eval(hole)
+        print(converted_hole)
+        for _key, _val in converted_hole.items():
+          new_holes = Holes(
+            game_id = game_id,
+            hole_number = _key,
+            par = _val
+          )
+          new_holes.save_to_db()
+      return {
+        'message': 'Holes info has ben saved'
+      }, 200
+
+    except:
+      return {
+        'message': 'Something went wrong'
+      }, 500
